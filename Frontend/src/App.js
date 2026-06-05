76c4640cc403ee239d2f30a94334d3084a519acf
shaikmaimoon09 <maimoon.shaik@inferlabs.ai>
2026-06-05 12:34:54 +0530

    React url

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,7 +5,7 @@ import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
   // baseURL: "http://localhost:8000",
-   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+   baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
 });
 
 function App() {
